954f30e2003ba5df6879768be4ec0a36e1f2f7db
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-09-11 21:14:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+function destructivelyUpdateObjectWithKeyAndvalue(object, key, value) {
+  object.[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  newObject = object
+  delete newObject[key]
+  return newObject
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
